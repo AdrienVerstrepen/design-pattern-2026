409ed22c9962e08bcc38c66c004b462351227237
obus325 <mathysbs@orange.fr>
2026-01-23 21:47:44 +0100

    travail séparation des responsabilités de Main

--- src/main/java/fr/fges/Main.java
+++ src/main/java/fr/fges/Main.java
@@ -2,25 +2,36 @@ package fr.fges;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length < 1) {
+        String storageFile = ArgumentReceiver(args);
+        ExtensionVerification(storageFile);
+        AllocateMemory(storageFile);
+        LaunchApplication();
+    }
+
+    public static String ArgumentReceiver(String[] arguments) {
+        if (arguments.length < 1) {
             System.out.println("Usage: java -jar boardgamecollection.jar <storage-file>");
             System.out.println("Storage file must be .json or .csv");
             System.exit(1);
         }
+        return arguments[0];
+    }
 
-        String storageFile = args[0];
-
-        // Check file extension
+    public static void ExtensionVerification(String storageFile) {
         if (!storageFile.endsWith(".json") && !storageFile.endsWith(".csv")) {
             System.out.println("Error: Storage file must have .json or .csv extension");
             System.exit(1);
         }
+    }
 
+    public static void AllocateMemory(String storageFile) {
         GameCollection.setStorageFile(storageFile);
         GameCollection.loadFromFile();
-
         System.out.println("Using storage file: " + storageFile);
+    }
 
+
+    public static void LaunchApplication() {
         while (true) {
             Menu.handleMenu();
         }
