10f0d76e3a6022438702542c8b17aabed9012335
Adrien Verstrepen <adrienverstrepen@gmail.com>
2026-02-06 14:13:48 +0100

    Ajouts de vérifs

--- src/main/java/fr/fges/services/Verifications/BoardGameVerificator.java
+++ src/main/java/fr/fges/services/Verifications/BoardGameVerificator.java
@@ -1,11 +1,23 @@
 package fr.fges.services.Verifications;
-import fr.fges.models.BoardGame;
 import fr.fges.repositories.GameCollectionDao;
 
-import static fr.fges.formatters.MenuFormatter.displayMessage;
 
 public class BoardGameVerificator {
 	public static boolean isADuplicate(String title, GameCollectionDao dao){
 		return dao.findAll().stream().anyMatch(game -> game.title().equalsIgnoreCase(title));
+	}
+
+	public static boolean isValidString(String input) {
+		// this function is there to test more easily the function verificationValidString without having to simulate user inputs
+		return input != null && !input.isBlank();
+	}
+
+	public static boolean isValidNumber(String input) {
+		try {
+			Integer.parseInt(input);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
 	}
 }
