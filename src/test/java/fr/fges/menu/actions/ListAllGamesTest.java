84f8eb58db5b03bd06bb5bf9af9188d26565a451
Adrien Verstrepen <adrienverstrepen@gmail.com>
2026-02-07 12:36:44 +0100

    Ajout test unitaire ListAllGamesTest

---
+++ src/test/java/fr/fges/menu/actions/ListAllGamesTest.java
@@ -0,0 +1,29 @@
+package fr.fges.menu.actions;
+
+import fr.fges.formatters.MenuInterface;
+import fr.fges.models.BoardGame;
+import fr.fges.repositories.GameCollectionDao;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+public class ListAllGamesTest {
+    @Test
+    void shouldDisplayGames() {
+        MenuInterface formatter = mock(MenuInterface.class);
+        GameCollectionDao dao = mock(GameCollectionDao.class);
+        List<BoardGame> games = List.of(
+                new BoardGame("J1", 1, 2, "Family"),
+                new BoardGame("J2", 3, 5, "Test"),
+                new BoardGame("J3", 2, 5, "Humor"),
+                new BoardGame("J4", 4, 6, "Fantasy")
+        );
+        when(dao.findAll()).thenReturn(games);
+        ListAllGamesEntry entry = new ListAllGamesEntry("List All Board Games");
+
+        entry.handle(formatter, dao);
+
+        verify(formatter).displayGames(games);
+    }
+}
