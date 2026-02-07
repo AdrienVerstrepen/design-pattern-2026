bb5c2c880f29358bdd0bf9090de3811fbdcb2bca
Adrien Verstrepen <adrienverstrepen@gmail.com>
2026-02-07 11:56:22 +0100

    Ajout AddGameTest

---
+++ src/test/java/fr/fges/menu/actions/AddGameTest.java
@@ -0,0 +1,64 @@
+package fr.fges.menu.actions;
+
+import fr.fges.formatters.MenuInterface;
+import fr.fges.models.BoardGame;
+import fr.fges.repositories.GameCollectionDao;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+public class AddGameTest {
+    @Test
+    void shouldTellUserGameIsADuplicate() {
+        MenuInterface formatter = mock(MenuInterface.class);
+        GameCollectionDao dao = mock(GameCollectionDao.class);
+        AddGameEntry entry = new AddGameEntry("Recommend by players");
+        when(dao.findByTitle(anyString())).thenReturn(Optional.of(new BoardGame("J1", 3, 4, "C1")));
+        when(formatter.getGameTitle()).thenReturn("J1");
+        when(formatter.getNumberFromUser(anyString())).thenReturn(2);
+        when(formatter.getGameCategory()).thenReturn("Family");
+
+        entry.handle(formatter, dao);
+
+        verify(formatter).displayMessage("A game with the same title already exists !");
+    }
+
+    @Test
+    void shouldTellUserGameHasNotBeenSaved() {
+        MenuInterface formatter = mock(MenuInterface.class);
+        GameCollectionDao dao = mock(GameCollectionDao.class);
+        AddGameEntry entry = new AddGameEntry("Recommend by players");
+        BoardGame notDuplicate = new BoardGame("J2", 3, 4, "C1");
+        when(dao.findByTitle(anyString())).thenReturn(Optional.empty());
+        when(formatter.getGameTitle()).thenReturn("J1");
+        when(formatter.getNumberFromUser(anyString())).thenReturn(2);
+        when(formatter.getGameCategory()).thenReturn("Family");
+        when(dao.save(new BoardGame("J1", 2, 2, "Family"))).thenReturn(true);
+
+        entry.handle(formatter, dao);
+
+        verify(formatter).displayMessage("Board game added successfully.");
+    }
+
+    @Test
+    void shouldConfirmGameIsAdded() {
+        MenuInterface formatter = mock(MenuInterface.class);
+        GameCollectionDao dao = mock(GameCollectionDao.class);
+        AddGameEntry entry = new AddGameEntry("Recommend by players");
+        BoardGame notDuplicate = new BoardGame("J2", 3, 4, "C1");
+        when(dao.findByTitle(anyString())).thenReturn(Optional.empty());
+        when(formatter.getGameTitle()).thenReturn("J1");
+        when(formatter.getNumberFromUser(anyString())).thenReturn(2);
+        when(formatter.getGameCategory()).thenReturn("Family");
+        when(dao.save(new BoardGame("J1", 2, 2, "Family"))).thenReturn(false);
+
+        entry.handle(formatter, dao);
+
+        verify(formatter).displayMessage("An error occurred, please try again.");
+    }
+}
