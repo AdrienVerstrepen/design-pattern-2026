b68442014c0608a5bc9e8b874f185c102e66430c
Adrien Verstrepen <adrienverstrepen@gmail.com>
2026-02-07 11:37:09 +0100

    Ajout vérification choix utilisateur

--- src/main/java/fr/fges/menu/Menu.java
+++ src/main/java/fr/fges/menu/Menu.java
@@ -35,6 +35,10 @@ public class Menu {
     public void handleMenu(List<MenuEntry> menuEntries) {
         formatter.displayMenu(menuEntries);
         int choice = formatter.getNumberFromUser("Please select an option (1-" + menuEntries.size() + "): ");
+        while(choice > menuEntries.size()) {
+            formatter.displayMessage("The number entered is invalid, please write a correct number.");
+            choice = formatter.getNumberFromUser("Please select an option (1-" + menuEntries.size() + "): ");
+        }
         formatter.displayMessage("");
         menuEntries.get(choice - 1).handle(formatter, dao);
         formatter.displayMessage("");
