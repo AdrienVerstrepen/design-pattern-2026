cb2a02d75594a314e5db382d5bdd28c425c55436
Adrien Verstrepen <adrienverstrepen@gmail.com>
2026-02-20 13:33:01 +0100

    Update classe Player

--- src/main/java/fr/fges/data/models/Player.java
+++ src/main/java/fr/fges/data/models/Player.java
@@ -1,8 +1,37 @@
 package fr.fges.data.models;
 
-public record Player(
-        String name,
-        int numberOfWins,
-        int points
-) {
+public class Player {
+    private String name;
+    private int numberOfWins;
+    private int points;
+
+    public Player(String name, int numberOfWins, int points) {
+        this.name = name;
+        this.numberOfWins = numberOfWins;
+        this.points = points;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfWins() {
+        return numberOfWins;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumberOfWins(int numberOfWins) {
+        this.numberOfWins = numberOfWins;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
 }
