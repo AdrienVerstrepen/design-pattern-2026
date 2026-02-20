f4ccedbfdaab225863e8be9b8e7a599789826135
obus325 <mathysbs@orange.fr>
2026-02-20 15:11:52 +0100

    Refactop UndoLastActionEntry

--- src/main/java/fr/fges/UI/menu/entriesUI/UndoLastActionEntry.java
+++ src/main/java/fr/fges/UI/menu/entriesUI/UndoLastActionEntry.java
@@ -1,26 +1,18 @@
 package fr.fges.UI.menu.entriesUI;
 import fr.fges.UI.formatters.MenuInterface;
-import fr.fges.data.commands.AddGameCommand;
-import fr.fges.data.commands.Command;
 import fr.fges.data.repositories.games.GameCollectionDao;
 import fr.fges.data.repositories.history.HistoryDao;
+import fr.fges.services.entriesServices.UndoLastActionService;
 
 import static fr.fges.services.verifications.BoardGameVerificator.isEmptyList;
 
-public record UndoLastActionEntry (String label, HistoryDao history, GameCollectionDao dao) implements MenuEntry {
+public record UndoLastActionEntry (String label, UndoLastActionService service, HistoryDao history, GameCollectionDao dao) implements MenuEntry {
     @Override
     public void handle(MenuInterface UI) {
-        if (isEmptyList(history.findAll())) {
-            UI.displayMessage("nothing to cancel");
-            return;
-        }
-        Command lastCommand = history.removeLast();
-        lastCommand.restore(dao);
-        if (lastCommand instanceof AddGameCommand) {
-            UI.displayMessage("Undone : Removed " + lastCommand.getModifiedGame().title() + " from collection");
-        } else {
-            UI.displayMessage("Undone : Added " + lastCommand.getModifiedGame().title() + " to collection");
-        }
+
+        UI.displayMessage(service.Undo(dao, history));
+
+
     }
 
 }
---
+++ src/main/java/fr/fges/services/entriesServices/UndoLastActionService.java
@@ -0,0 +1,28 @@
+package fr.fges.services.entriesServices;
+
+import fr.fges.data.commands.AddGameCommand;
+import fr.fges.data.commands.Command;
+import fr.fges.data.models.BoardGame;
+import fr.fges.data.repositories.games.GameCollectionDao;
+import fr.fges.data.repositories.history.HistoryDao;
+
+import static fr.fges.services.verifications.BoardGameVerificator.isEmptyList;
+
+
+public class UndoLastActionService{
+	public String Undo(GameCollectionDao dao, HistoryDao history) {
+		if (isEmptyList(history.findAll())) {
+			return "nothing to cancel";
+		}
+
+		Command lastCommand = history.removeLast();
+
+		lastCommand.restore(dao);
+
+		if (lastCommand instanceof AddGameCommand) {
+			return "Undone : Removed " + lastCommand.getModifiedGame().title() + " from collection";
+		} else {
+			return "Undone : Added " + lastCommand.getModifiedGame().title() + " to collection";
+		}
+	}
+}
