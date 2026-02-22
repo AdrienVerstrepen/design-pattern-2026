5f4713c9603f2b118710a557dd8a8e9958c1ed7e
Adrien Verstrepen <adrienverstrepen@gmail.com>
2026-02-22 15:31:04 +0100

    Ajout tests UI RecommendByPlayerCountTest

--- src/test/java/fr/fges/UI/menu/entriesUI/RecommendByPlayerCountTest.java
+++ src/test/java/fr/fges/UI/menu/entriesUI/RecommendByPlayerCountTest.java
@@ -2,42 +2,57 @@ package fr.fges.UI.menu.entriesUI;
 import fr.fges.UI.formatters.MenuInterface;
 import fr.fges.data.models.BoardGame;
 import fr.fges.data.repositories.games.GameCollectionDao;
+import fr.fges.services.entriesServices.RecommendByPlayerCountService;
+import fr.fges.services.results.Failure;
+import fr.fges.services.results.Success;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import static org.mockito.Mockito.*;
 
 class RecommendByPlayerCountTest {
-//    @Test
-//    void handleValid() {
-//        MenuInterface formatter = mock(MenuInterface.class);
-//        GameCollectionDao dao = mock(GameCollectionDao.class);
-//        RecommendByPlayerCountEntry entry = new RecommendByPlayerCountEntry("Recommend by players");
-//        List<BoardGame> games = List.of(
-//                new BoardGame("J1", 3, 4, "C1"),
-//                new BoardGame("J2", 2, 10, "C2")
-//        );
-//        when(formatter.getNumberFromUser("Number of players: ")).thenReturn(4);
-//        when(dao.findAllInAlphabeticalOrder(anyList())).thenReturn(games);
-//
-//        entry.handle(formatter, dao);
-//
-//        verify(formatter).displayGames(games);
-//        verify(formatter, never()).displayMessage(anyString());
-//    }
-//
-//    @Test
-//    void handleInvalid() {
-//        MenuInterface formatter = mock(MenuInterface.class);
-//        GameCollectionDao dao = mock(GameCollectionDao.class);
-//        RecommendByPlayerCountEntry entry = new RecommendByPlayerCountEntry("Recommend by players");
-//        when(formatter.getNumberFromUser("Number of players: ")).thenReturn(5);
-//        when(dao.findAllInAlphabeticalOrder(anyList())).thenReturn(Collections.emptyList());
-//
-//        entry.handle(formatter, dao);
-//
-//        verify(formatter).displayMessage("There is no game for this number of players.");
-//        verify(formatter, never()).displayGames(any());
-//    }
+    @Test
+    void shouldDisplayGames() {
+        MenuInterface formatter = mock(MenuInterface.class);
+        RecommendByPlayerCountService service =  mock(RecommendByPlayerCountService.class);
+        List<BoardGame> games = List.of(
+                new BoardGame("J1", 1, 2, "Family"),
+                new BoardGame("J2", 3, 5, "Test"),
+                new BoardGame("J3", 2, 5, "Humor"),
+                new BoardGame("J4", 4, 6, "Fantasy")
+        );
+        when(service.recommendByPlayerCount(anyInt())).thenReturn(new Success<>(games));
+        RecommendByPlayerCountEntry entry = new RecommendByPlayerCountEntry("Recommend games for a number of players", service);
+
+        entry.handle(formatter);
+
+        verify(formatter).displayGames(games);
+    }
+
+    @Test
+    void shouldDisplayNoGamesAvailable() {
+        MenuInterface formatter = mock(MenuInterface.class);
+        RecommendByPlayerCountService service =  mock(RecommendByPlayerCountService.class);
+        when(service.recommendByPlayerCount(anyInt())).thenReturn(new Failure<>("The number of players must be bigger than zero."));
+        RecommendByPlayerCountEntry entry = new RecommendByPlayerCountEntry("Recommend games for a number of players", service);
+
+        entry.handle(formatter);
+
+        verify(formatter).displayMessage("The number of players must be bigger than zero.");
+    }
+
+    @Test
+    void shouldDisplayInvalidNumberChosen() {
+        MenuInterface formatter = mock(MenuInterface.class);
+        RecommendByPlayerCountService service =  mock(RecommendByPlayerCountService.class);
+        when(service.recommendByPlayerCount(anyInt())).thenReturn(new Failure<>("There is no game for this number of players."));
+        RecommendByPlayerCountEntry entry = new RecommendByPlayerCountEntry("Recommend games for a number of players", service);
+
+        entry.handle(formatter);
+
+        verify(formatter).displayMessage("There is no game for this number of players.");
+    }
+
 }
