4fa93721414b0d47c0c227883641dadb399e3828
Adrien Verstrepen <adrienverstrepen@gmail.com>
2026-02-22 15:21:51 +0100

    Ajout tests recommendOneGameUI et RemoveGameUI

--- src/test/java/fr/fges/UI/menu/entriesUI/RecommendOneGameTest.java
+++ src/test/java/fr/fges/UI/menu/entriesUI/RecommendOneGameTest.java
@@ -1,19 +1,57 @@
 package fr.fges.UI.menu.entriesUI;
 
 import fr.fges.UI.formatters.MenuInterface;
+import fr.fges.data.models.BoardGame;
 import fr.fges.data.repositories.games.GameCollectionDao;
+import fr.fges.services.entriesServices.RecommendOneGameService;
+import fr.fges.services.results.Failure;
+import fr.fges.services.results.Success;
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class RecommendOneGameTest {
-//    @Test
-//    void shouldDisplayOneGame() {
-//        MenuInterface formatter = mock(MenuInterface.class);
-//        GameCollectionDao dao = mock(GameCollectionDao.class);
-//        when(formatter.getNumberFromUser(anyString())).thenReturn(3);
-//        when(dao.delete("Catan")).thenReturn(true);
-//    }
+    @Test
+    void shouldDisplayOneGame() {
+        int n = 4;
+        MenuInterface formatter = mock(MenuInterface.class);
+        when(formatter.getNumberFromUser(anyString())).thenReturn(n);
+        RecommendOneGameService service = mock(RecommendOneGameService.class);
+        GameCollectionDao gamesDao = mock(GameCollectionDao.class);
+        BoardGame game = new BoardGame("J1", n, n+1, "Humor");
+        when(service.recommendOneGame(n, gamesDao)).thenReturn(new Success<>(game));
+
+        RecommendOneGameEntry entry = new RecommendOneGameEntry("J1", gamesDao, service);
+        entry.handle(formatter);
+
+        verify(formatter).displayGame(game);
+    }
+
+    @Test
+    void shouldDisplayInvalidNumberChosen() {
+        MenuInterface formatter = mock(MenuInterface.class);
+        RecommendOneGameService service = mock(RecommendOneGameService.class);
+        GameCollectionDao gamesDao = mock(GameCollectionDao.class);
+        when(service.recommendOneGame(anyInt(), any(GameCollectionDao.class))).thenReturn(new Failure<>("Number of players must be greater than zero."));
+
+        RecommendOneGameEntry entry = new RecommendOneGameEntry("J1", gamesDao, service);
+        entry.handle(formatter);
+
+        verify(formatter).displayMessage("Number of players must be greater than zero.");
+    }
+
+    @Test
+    void shouldDisplayNoGamesAvailable() {
+        MenuInterface formatter = mock(MenuInterface.class);
+        RecommendOneGameService service = mock(RecommendOneGameService.class);
+        GameCollectionDao gamesDao = mock(GameCollectionDao.class);
+        when(service.recommendOneGame(anyInt(), any(GameCollectionDao.class))).thenReturn(new Failure<>("No games available for this number of players."));
+
+        RecommendOneGameEntry entry = new RecommendOneGameEntry("J1", gamesDao, service);
+        entry.handle(formatter);
+
+        verify(formatter).displayMessage("No games available for this number of players.");
+
+
+    }
 }
--- src/test/java/fr/fges/UI/menu/entriesUI/RemoveGameTest.java
+++ src/test/java/fr/fges/UI/menu/entriesUI/RemoveGameTest.java
@@ -3,6 +3,7 @@ import fr.fges.UI.formatters.MenuInterface;
 import fr.fges.data.models.BoardGame;
 import fr.fges.data.repositories.games.GameCollectionDao;
 import fr.fges.data.repositories.history.HistoryDao;
+import fr.fges.services.entriesServices.RemoveGameService;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
@@ -10,34 +11,37 @@ import java.util.Optional;
 import static org.mockito.Mockito.*;
 
 public class RemoveGameTest {
-//    @Test
-//    void shouldTellUserGameHasBeenRemoved () {
-//        MenuInterface formatter = mock(MenuInterface.class);
-//        GameCollectionDao dao = mock(GameCollectionDao.class);
-//        HistoryDao history = mock(HistoryDao.class);
-//        when(formatter.getGameTitle()).thenReturn("Catan");
-//        when(dao.delete("Catan")).thenReturn(true);
-//        when(dao.findByTitle(anyString())).thenReturn(Optional.of(new BoardGame("Catan", 1, 5, "fam")));
-//
-//        RemoveGameEntry entry = new RemoveGameEntry("Remove Board Game", history);
-//
-//        entry.handle(formatter, dao);
-//
-//        verify(formatter).displayMessage("Board game removed successfully.");
-//    }
-//
-//    @Test
-//    void shouldTellUserNoGameToRemove () {
-//        MenuInterface formatter = mock(MenuInterface.class);
-//        GameCollectionDao dao = mock(GameCollectionDao.class);
-//        HistoryDao history = mock(HistoryDao.class);
-//        when(formatter.getGameTitle()).thenReturn("Catan");
-//        when(dao.findByTitle(anyString())).thenReturn(Optional.empty());
-//        when(dao.delete("Catan")).thenReturn(false);
-//        RemoveGameEntry entry = new RemoveGameEntry("Remove Board Game", history);
-//
-//        entry.handle(formatter, dao);
-//
-//        verify(formatter).displayMessage("No board game found with that title.");
-//    }
+    @Test
+    void shouldTellUserGameHasBeenRemoved () {
+        MenuInterface formatter = mock(MenuInterface.class);
+        GameCollectionDao dao = mock(GameCollectionDao.class);
+        HistoryDao history = mock(HistoryDao.class);
+        when(formatter.getGameTitle()).thenReturn("Catan");
+        when(dao.delete("Catan")).thenReturn(true);
+        when(dao.findByTitle(anyString())).thenReturn(Optional.of(new BoardGame("Catan", 1, 5, "fam")));
+        RemoveGameService service = new RemoveGameService(dao, history);
+
+        RemoveGameEntry entry = new RemoveGameEntry("Remove Board Game", service);
+
+        entry.handle(formatter);
+
+        verify(formatter).displayMessage("Board game removed successfully.");
+    }
+
+    @Test
+    void shouldTellUserNoGameToRemove () {
+        MenuInterface formatter = mock(MenuInterface.class);
+        GameCollectionDao dao = mock(GameCollectionDao.class);
+        HistoryDao history = mock(HistoryDao.class);
+        when(formatter.getGameTitle()).thenReturn("Catan");
+        when(dao.findByTitle(anyString())).thenReturn(Optional.empty());
+        when(dao.delete("Catan")).thenReturn(false);
+        RemoveGameService service = new RemoveGameService(dao, history);
+
+        RemoveGameEntry entry = new RemoveGameEntry("Remove Board Game", service);
+
+        entry.handle(formatter);
+
+        verify(formatter).displayMessage("No board game found with that title.");
+    }
 }
